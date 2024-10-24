b6804c31169743bf57b689fbca7bab2a6a658297
Sam <oyedeji.smt@gmail.com>
2024-10-23 23:13:13 -0500

    added alphabet search

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,13 +1,22 @@
 class BreedsController < ApplicationController
   def index
-    @breeds = Breed.page(params[:page]).per(9) # Display 9 breeds per page
-  end
-  def search
-    @breeds = Breed.where("name LIKE ?", "%#{params[:query]}%")
-    render :index
+    if params[:letter]
+      # Filter breeds by the first letter
+      @breeds = Breed.where("name LIKE ?", "#{params[:letter]}%").page(params[:page]).per(9)
+    elsif params[:query]
+      # Search breeds by query
+      @breeds = Breed.where("name LIKE ?", "%#{params[:query]}%").page(params[:page]).per(9)
+    else
+      # Display all breeds
+      @breeds = Breed.page(params[:page]).per(9)
+    end
   end
 
   def show
     @breed = Breed.find(params[:id])
+  end
+
+  def search
+    @breeds = Breed.where("name LIKE ?", "%#{params[:query]}%")
   end
 end
